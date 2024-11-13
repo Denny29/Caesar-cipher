70c1e797f2c6a2a3e047fe759cc007246344dd9d
Denny Fernandez <dennyrfernandez2004@gmail.com>
2024-11-12 19:27:54 -0500

    Did the cipher wrong, was shifting to left instead of to the right. Fixed now

--- Caesar_Cipher.java
+++ Caesar_Cipher.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Caesar_Cipher {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        caesar(null, 3);
+        caesar("cat", 23);
         // A = 65
         // Z = 90
         // a = 97
@@ -13,7 +14,7 @@ public class Caesar_Cipher {
 
     /**
      * Returns a String with each character shifted {key} times to the left
-     * EX: caesar("cat", 3) will return "Zxq"
+     * EX: caesar("cat", 23) will return "Zxq"
      * 
      * @param cipher The cipher to be encrypted
      * @param key    The number of times each character is shifted
@@ -22,30 +23,38 @@ public class Caesar_Cipher {
         // Make a hashmap containing what each character should be
         HashMap<Character, Character> legend = new HashMap<Character, Character>();
 
-        // Actually make the shift
+        // Fill the hashmap with the right keys.
         for (Character upperCase = 'A', lowerCase = 'a'; upperCase != 'Z'; upperCase++, lowerCase++) {
 
-            int upperShift = (int) upperCase - key;
-            int lowerShift = (int) lowerCase - key;
+            int upperShift = (int) upperCase + key;
+            int lowerShift = (int) lowerCase + key;
 
             // These if statements make sure that when the letters go "below" A or a they go
             // back to Z or z.
 
-            if ((int) upperShift < 65) {
-                upperShift += 26;
+            if ((int) upperShift > 90) {
+                upperShift -= 26;
             }
 
-            if ((int) lowerShift < 97) {
-                lowerShift += 26;
+            if ((int) lowerShift > 122) {
+                lowerShift -= 26;
             }
 
             legend.put(upperCase, (char) upperShift);
             legend.put(lowerCase, (char) lowerShift);
         }
 
-        System.out.println(legend.keySet());
-        System.out.println(legend.values());
-
+        char[] charArray = cipher.toCharArray();
+        String encryptedCipher = "";
+        // Iterate over the charArray using the legend to shift it
+        for (char c : charArray) {
+            if (c == ' ') {
+                encryptedCipher += " ";
+            } else {
+                encryptedCipher += legend.get(c);
+            }
+        }
+        System.out.println(encryptedCipher);
         return null;
     }
 }
